6112e612eca5aa89d2707fcceb50f50463177625
Serena-19 <serena.schincaglia.11-2@accademiadigitaleliguria.it>
2022-09-29 14:15:15 +0200

    Update Cliente.py

--- Codice/Cliente.py
+++ Codice/Cliente.py
@@ -79,13 +79,7 @@ class App_eporfoglio:
         self.lista_conti = {}
         self.lista_clienti = {}
 
-    @property
-    def nome(self):
-    return self.__nome
-
-    @nome.setter
-    def nome(self, nome):
-        self.__nome = nome
+    
 
 
 
